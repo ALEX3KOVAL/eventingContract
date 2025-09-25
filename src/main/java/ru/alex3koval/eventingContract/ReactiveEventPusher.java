a4906ff6755ebda4f489d5e309a6674010b864a4
Alexandr Kovalenko <destructsymp@gmail.com>
2025-09-26 00:59:23 +0300

    main: some changes.

--- src/main/java/ru/alex3koval/eventingContract/ReactiveEventPusher.java
+++ src/main/java/ru/alex3koval/eventingContract/ReactiveEventPusher.java
@@ -3,7 +3,7 @@ package ru.alex3koval.eventingContract;
 import reactor.core.publisher.Mono;
 import ru.alex3koval.eventingContract.vo.EventStatus;
 
-@FunctionalInterface
 public interface ReactiveEventPusher<T> extends BaseEventPusher {
     Mono<T> push(String topic, EventStatus status, Object payload);
+    Mono<T> push(String topic, EventStatus status, Object payload, String eventName);
 }
--- src/main/java/ru/alex3koval/eventingContract/SyncEventPusher.java
+++ src/main/java/ru/alex3koval/eventingContract/SyncEventPusher.java
@@ -2,7 +2,7 @@ package ru.alex3koval.eventingContract;
 
 import ru.alex3koval.eventingContract.vo.EventStatus;
 
-@FunctionalInterface
 public interface SyncEventPusher extends BaseEventPusher {
     void push(String topic, EventStatus status, Object payload) throws InterruptedException;
+    void push(String topic, EventStatus status, Object payload, String eventName);
 }
