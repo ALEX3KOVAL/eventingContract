8cfa0cb8a5c612bd962cdf2c66ea7d1782d819b3
Alexandr Kovalenko <destructsymp@gmail.com>
2025-09-25 19:04:40 +0300

    main: new version.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,10 +3,3 @@ plugins {
 }
 
 rootProject.name = "eventingContract"
-
-dependencyResolutionManagement {
-    repositories {
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-    }
-}
