4897a4bdaa5b1a52e99b855bfb4333e8ed84f55e
Alexandr Kovalenko <destructsymp@gmail.com>
2025-10-12 13:44:44 +0300

    main: map.

--- src/main/java/ru/alex3koval/eventingContract/dto/CreateEventWDTO.java
+++ src/main/java/ru/alex3koval/eventingContract/dto/CreateEventWDTO.java
@@ -3,11 +3,12 @@ package ru.alex3koval.eventingContract.dto;
 import ru.alex3koval.eventingContract.vo.EventStatus;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 public record CreateEventWDTO(
     String name,
     String topic,
-    String json,
+    Map<String, Object> model,
     EventStatus status,
     LocalDateTime createdAt
 ) {
