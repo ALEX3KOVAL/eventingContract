2f9ecfdb67b6cb5f0c7e3992bfa73950a940d46e
Alexandr Kovalenko <destructsymp@gmail.com>
2025-10-22 02:43:20 +0300

    main: add id in event entity.

--- src/main/java/ru/alex3koval/eventingContract/Event.java
+++ src/main/java/ru/alex3koval/eventingContract/Event.java
@@ -1,22 +1,25 @@
 package ru.alex3koval.eventingContract;
 
-import ru.alex3koval.eventingContract.vo.EventStatus;
 import lombok.Getter;
+import ru.alex3koval.eventingContract.vo.EventStatus;
 
 import java.time.LocalDateTime;
 
 @Getter
 public class Event {
+    private final String id;
     private final String name;
     private final String json;
     private final EventStatus status;
     private final LocalDateTime createdAt;
 
     public Event(
+        String id,
         String name,
         String json,
         EventStatus status
     ) {
+        this.id = id;
         this.name = name;
         this.json = json;
         this.status = status;
@@ -24,11 +27,13 @@ public class Event {
     }
 
     public Event(
+        String id,
         String name,
         String json,
         EventStatus status,
         LocalDateTime createdAt
     ) {
+        this.id = id;
         this.name = name;
         this.json = json;
         this.status = status;
