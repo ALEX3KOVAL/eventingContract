be843816ccf08ab4524adaa0898f7bbd085aa413
Alexandr Kovalenko <destructsymp@gmail.com>
2025-10-22 03:33:40 +0300

    main: del updatedAt prop from updating dto.

--- src/main/java/ru/alex3koval/eventingContract/dto/UpdateEventWDTO.java
+++ src/main/java/ru/alex3koval/eventingContract/dto/UpdateEventWDTO.java
@@ -2,10 +2,7 @@ package ru.alex3koval.eventingContract.dto;
 
 import ru.alex3koval.eventingContract.vo.EventStatus;
 
-import java.time.LocalDateTime;
-
 public record UpdateEventWDTO(
-    EventStatus status,
-    LocalDateTime updatedAt
+    EventStatus status
 ) {
 }
