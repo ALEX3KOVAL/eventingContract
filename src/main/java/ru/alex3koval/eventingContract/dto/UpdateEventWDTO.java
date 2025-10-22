b688cb23dda5730c9df075ce30ec2c7a6d29c4af
Alexandr Kovalenko <destructsymp@gmail.com>
2025-10-22 03:14:55 +0300

    main: add status in updating dto.

--- src/main/java/ru/alex3koval/eventingContract/dto/UpdateEventWDTO.java
+++ src/main/java/ru/alex3koval/eventingContract/dto/UpdateEventWDTO.java
@@ -1,8 +1,11 @@
 package ru.alex3koval.eventingContract.dto;
 
+import ru.alex3koval.eventingContract.vo.EventStatus;
+
 import java.time.LocalDateTime;
 
 public record UpdateEventWDTO(
+    EventStatus status,
     LocalDateTime updatedAt
 ) {
 }
